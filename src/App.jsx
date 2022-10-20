5222bb4bf41ca0d786cbc7825d049934ab8545ae
Ebbe# <ebenezarbukosia@gmail.com>
2022-10-20 15:58:45 +0300

    Dev (#13)
    * Landing page Navbar #6
    
    * [FIX] intergrate navbar with rest of design
    
    Co-authored-by: allanstar-byte <allanmutisya263@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Home from "./pages/Home";
-import Solutions from "./pages/Solutions";
 import "./sass/style.scss";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Solutions from "./pages/Solutions";
 
 function App() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <Solutions />
     </>
   );
 }
