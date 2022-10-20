f4f036661dc25a13bde45b804b1710678b9178d1
allanstar-byte <allanmutisya263@gmail.com>
2022-10-20 16:09:25 +0300

    Landing page WelcomeMessage #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import "./sass/style.scss";
 import React from "react";
 import Landing from "./pages/Landing";
+import Solutions from "../src/pages/Solutions";
+
 function App() {
   return (
     <>
       <Landing/>
+      {/* <Solutions /> */}
     </>
   )
 }
