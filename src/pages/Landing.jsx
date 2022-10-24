3341c80c4420883355ba64cd5e03280e523edc89
allanstar-byte <allanmutisya263@gmail.com>
2022-10-24 13:29:03 +0300

    Registration Form #6

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import Belt from "../components/Belt";
 import LandingFooter from "../components/Landing-Footer";
+import Register from "../components/Registry";
 import Welcome from "../components/WelcomeMessage";
 
 function Landing() {
   return (
     <div>
       <Welcome />
+      <Register/>
       <Belt />
       <LandingFooter />
     </div>
