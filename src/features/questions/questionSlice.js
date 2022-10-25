cb68d021c11ab6a7cb12bd4f9a282cb9f5266ba9
Ebenezr <ebenezarbukosia@gmail.com>
2022-10-25 16:39:06 +0300

    [FIX] minor fixes

--- src/features/questions/questionSlice.js
+++ src/features/questions/questionSlice.js
@@ -17,8 +17,6 @@ export const getQuestions = createAsyncThunk(
 
 export const postQuestions = createAsyncThunk(
   "questions/addnewQuestion",
-  async (formData) => {
-    try {
   async (formData, thunkAPI) => {
     try {
       const responce = await axios.post(url, formData);
